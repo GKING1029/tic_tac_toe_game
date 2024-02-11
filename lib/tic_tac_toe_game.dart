056c0cfc43c646225511e26d3649ce4c7293d7a0
Gurjot Singh <95275780+GKING1029@users.noreply.github.com>
2024-02-11 16:41:03 +0530

    Initial commit

---
+++ lib/tic_tac_toe_game.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class TicTacToe extends StatefulWidget {
+  const TicTacToe({super.key});
+
+  @override
+  State<TicTacToe> createState() => _TicTacToeState();
+}
+
+class _TicTacToeState extends State<TicTacToe> {
+  bool isCompleted = false;
+  List<String> game = List.filled(9, "");
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.lightBlue,
+          title: const Center(
+            child: Text(
+              "Tic Tac Toe",
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+          )),
+      body: GridView.builder(
+        shrinkWrap: true,
+        itemCount: 9,
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              if (game[index].isEmpty) {
+                setState(() {
+                  game[index] = isCompleted ? 'X' : 'O';
+                });
+              }
+            },
+            onDoubleTap: () {
+              if (game[index].isEmpty) {
+                setState(() {
+                  game[index] = isCompleted ? 'O' : 'X';
+                });
+              }
+            },
+            child: Container(
+              color: Colors.black,
+              child: Center(
+                child: Text(
+                  game[index],
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 100,
+                      color: Colors.lightBlue),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
